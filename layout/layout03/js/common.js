f31651c957fc01f8182a91ef5a28d01da036c85c
eve <92919741+seoyeonAn@users.noreply.github.com>
2022-03-27 01:07:06 +0900

    0

--- layout/layout03/js/common.js
+++ layout/layout03/js/common.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 
     $(window).scroll(function(){
-        if($(this).scrollTop() < 5){
-            $('#top').animate({'fliter':'blur, 5px'},)
+        if($(this).scrollTop() > 2){
+            $('#top').stop().animate({'height':'120px'},300);
+            $('.topimg').stop().animate({'height':'100px','filter':'blur,2px'},300);
+        }else {
+            $('#top').stop().animate({'height':'220px'},300);
+            $('.topimg').stop().animate({'height':'200px'},300);
         }
     })
 });
