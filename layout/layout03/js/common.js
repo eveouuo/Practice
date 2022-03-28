68f02cfaaf6619ab2e527ffd93373277b55f099e
eve <lovelylove.eve@gmail.com>
2022-03-28 17:54:25 +0900

    .

--- layout/layout03/js/common.js
+++ layout/layout03/js/common.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if($(this).scrollTop() > 2){
             $('#top').stop().animate({'height':'120px'},300);
-            $('.topimg .imgbox').stop().animate({'height':'100px','filter':'blur,2px'},300);
+            $('.topimg .imgbox').stop().animate({'height':'100px'},300);
         }else {
             $('#top').stop().animate({'height':'220px'},300);
             $('.topimg .imgbox').stop().animate({'height':'200px'},300);
